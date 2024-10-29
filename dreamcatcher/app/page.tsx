b4270799603bc9e22efba46a772c0528c4f92332
Öykü Kaya <oykuky001@gmail.com>
2024-10-30 00:51:44 +0300

    updated

--- dreamcatcher/app/page.tsx
+++ dreamcatcher/app/page.tsx
@@ -3,7 +3,7 @@ import { AnimatedBackground } from 'animated-backgrounds';
 import DreamForm from './components/DreamForm';
 export default function Home() {
   return (
-    <div className="text-center">
+    <div className="flex text-center justify-center items-center">
       <AnimatedBackground animationName="cosmicDust" style={{ opacity: 0.5 }} />
       <DreamForm/>
     </div>
