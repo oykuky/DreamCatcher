a20b55413e0ce9b2cc37a7472239e959ea1b5e03
Öykü Kaya <oykuky001@gmail.com>
2024-10-31 00:23:30 +0300

    updated

--- dreamcatcher/app/components/DreamForm.tsx
+++ dreamcatcher/app/components/DreamForm.tsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function DreamForm() {
+  // const [keywords,setKeywords] = useState<string[]>([]);
+  const [dreamTxt,setDreamTxt] = useState<string>("");
+
+  // const handleKeywordsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const inputValue = e.target.value;
+  //   setKeywords(inputValue.split(",").map((keyword) => keyword.trim()));
+  // };
+  
   return (
-    <div className="w-full max-w-[800px] px-2  mt-20 flex gap-4 p-1.5 bg-gradient-to-r from-purple-600 via-fuchsia-500 to-blue-800 m-3 rounded-md justify-center items-center">
-      <div className="rounded-md p-2 bg-gray-900 w-full">
+    <div className="w-full max-w-[800px] px-2  mt-20 flex gap-4 p-1.5 bg-gradient-to-r from-fuchsia-500 via-purple-700 to-blue-800 m-3 rounded-md justify-center items-center">
+      <div className="rounded-md p-2 bg-gray-950 w-full">
         <form className="flex flex-col gap-8 p-2 ">
             <label className="text-lg font-medium">Rüyanızı Anlatın</label>
             <textarea
               placeholder="Rüyanızı detaylı bir şekilde anlatın..."
-              // value={}
-              // onChange={}
-              className="min-h-32 bg-gray-800 rounded-lg focus:border-yellow-400 focus:border-2 outline-none"
+              value={dreamTxt}
+              onChange={(e)=>setDreamTxt(e.target.value)}
+              className="min-h-32 bg-gray-900 rounded-lg focus:border-yellow-400 focus:border-2 outline-none"
             />
         
             <label className="text-lg font-medium">Anahtar Kelimeler</label>
             <input
-              className="bg-gray-800 rounded-lg focus:border-yellow-400 focus:border-2 outline-none"
+              className="bg-gray-900 rounded-lg focus:border-yellow-400 focus:border-2 outline-none"
               placeholder="Örn: uçmak, deniz, aile (virgülle ayırın)"
-              // value={keywords}
-              // onChange={(e) => setKeywords(e.target.value)}
+              // onChange={handleKeywordsChange}
+             
             />
           <button type="submit" className="w-full h-9 font-semibold bg-fuchsia-800 rounded-md">
             Rüyayı Yorumla
--- dreamcatcher/app/components/Header.tsx
+++ dreamcatcher/app/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () =>{
   return (
-    <div className="bg-gray-900 h-36 rounded-md m-3 border-[4px] border-purple-800 md:mx-10">
+    <div className="bg-gray-950 h-36 rounded-md m-3 border-[4px] border-purple-800 md:mx-10">
         <div className='flex flex-col text-center font-bold mt-4 gap-5'>
                <div className='flex justify-center items-center'>
                   <Image src="/dream-catcher.png" alt='dream-catcher' width={50} height={50} className='rounded-full bg-purple-500 p-1 mr-3'/>
